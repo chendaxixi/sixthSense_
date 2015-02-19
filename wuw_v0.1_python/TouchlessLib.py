3fd12d5306523bed853bd7636a6ca82d477e7950
chendaxixi <215bcsb342@gmail.com>
2015-02-20 02:23:05 +0800

    Add Camera

--- wuw_v0.1_python/TouchlessLib.py
+++ wuw_v0.1_python/TouchlessLib.py
@@ -3,5 +3,97 @@
 #TouchlessLib模块定义
 ##标记物识别
 
+import threading
+import cv2.cv as cv
+import Image
+import wx
+
+#摄像头类定义
 class Camera:
     
+    def __init__(self, name="camera"):
+        self.__name = name
+        self.__height = 480
+        self.__width = 640
+        self.__img = None
+        self.__imgLock = threading.RLock()
+        self.__fpslimit = 30
+        self.__cam = None
+
+    @property
+    def CaptureHeight(self):
+        return self.__height
+    @CaptureHeight.setter
+    def CaptureHeight(self, value):
+        self.__height = value
+
+    @property
+    def CaptureWidth(self):
+        return self.__width
+    @CaptureWidth.setter
+    def CaptureWidth(self, value):
+        self.__width = value
+
+    @property
+    def Fps(self):
+        return self.__fpslimit
+    @Fps.setter
+    def Fps(self, value):
+        self.__fpslimit = value
+
+    def ToString(self):
+        return self.__name
+
+    def Start(self):
+        if not self.isOn():
+            self.__cam = cv.CaptureFromCAM(0)
+
+    def Stop(self):
+        if self.isOn():
+            self.__cam = cv.CaptureFromCAM(0)
+            self.__cam = None
+
+    def isOn(self):
+        if self.__cam == None:
+            return False
+        else:
+            return True
+
+    def ImageCaptured(self):
+        self.__imgLock.acquire()
+        img = cv.QueryFrame(self.__cam)
+        self.__img = Image.fromstring("RGB", (self.__width, self.__height), img.tostring())
+        self.__imgLock.release()
+
+    def GetCurrentImage(self):
+        self.__imgLock.acquire()
+        if self.__img == None:
+            img = None
+        else:
+            img = self.__img.copy()
+        self.__imgLock.release()
+        return img
+
+    def Dispose(self):
+        self.Stop()
+        self.__img = None
+
+#Image To Bitmap
+def ImageToBitmap(img):
+    img2 = wx.EmptyImage(img.size[0], img.size[1])
+    img2.SetData(img.tostring())
+    bitmap = wx.BitmapFromImage(img2)
+    return bitmap
+
+#HSV类定义
+class HSV:
+    pass
+
+#RGB类定义
+
+class RGB:
+    pass
+
+#Marker类定义
+class Marker:
+    pass
--- wuw_v0.1_python/wuw.py
+++ wuw_v0.1_python/wuw.py
@@ -12,6 +12,9 @@
 
 import wx
 from classes.Value import Value
+import threading
+import TouchlessLib
+import time
 
 class WuwPanel(wx.Panel):
     Width = Value.WuwWidth
@@ -21,123 +24,163 @@ class WuwPanel(wx.Panel):
         wx.Panel.__init__(self, parent)
 
         #构建TabPage构件组
-        wx.btnShowHide=wx.Button(self,pos=(self.Width-self.Grid*5,self.Grid),
+        self.btnShowHide=wx.Button(self,pos=(self.Width-self.Grid*5,self.Grid),
                                  size=(3*self.Grid,3*self.Grid))
-        wx.btnExit=wx.Button(self,label="Exit",pos=(self.Width-self.Grid*9,self.Grid),
+        self.btnExit=wx.Button(self,label="Exit",pos=(self.Width-self.Grid*9,self.Grid),
                              size=(3*self.Grid,3*self.Grid))
-        wx.tabSettings=wx.Notebook(self,pos=(self.Width-self.Grid*62,self.Grid*3),
+        self.tabSettings=wx.Notebook(self,pos=(self.Width-self.Grid*62,self.Grid*3),
                                    size=(60*self.Grid,20*self.Grid))
-        wx.pictureBoxDisplay=wx.StaticBox(self,pos=(self.Width-self.Grid*62,self.Grid*23),
-                                             size=(60*self.Grid,50*self.Grid))
-        wx.tabSettings.SetTabSize((self.Grid,1.5*self.Grid))
-        wx.tabPageCamera=wx.Panel(wx.tabSettings)
-        wx.tabPageTokens=wx.Panel(wx.tabSettings)
-        wx.tabPageApps=wx.Panel(wx.tabSettings)
-        wx.tabSettings.AddPage(wx.tabPageCamera, "Camera")
-        wx.tabSettings.AddPage(wx.tabPageTokens, "Tokens")
-        wx.tabSettings.AddPage(wx.tabPageApps, "Apps")
+        self.pictureBoxDisplay=wx.StaticBox(self,pos=(self.Width-self.Grid*66,self.Grid*23),
+                                             size=(64*self.Grid,48*self.Grid))
+        self.tabSettings.SetTabSize((self.Grid,1.5*self.Grid))
+        self.tabPageCamera=wx.Panel(self.tabSettings)
+        self.tabPageTokens=wx.Panel(self.tabSettings)
+        self.tabPageApps=wx.Panel(self.tabSettings)
+        self.tabSettings.AddPage(self.tabPageCamera, "Camera")
+        self.tabSettings.AddPage(self.tabPageTokens, "Tokens")
+        self.tabSettings.AddPage(self.tabPageApps, "Apps")
         ##构建TabPageCamera
-        wx.comboBoxCameras=wx.ComboBox(wx.tabPageCamera,value="Select A Camera",
+        self.comboBoxCameras=wx.ComboBox(self.tabPageCamera,value="Select A Camera",
                                        pos=(0,0), size=(25*self.Grid,2*self.Grid))
-        wx.lblCameraInfo=wx.StaticText(wx.tabPageCamera,label="No Camera Selected",
+        self.lblCameraInfo=wx.StaticText(self.tabPageCamera,label="No Camera Selected",
                                        pos=(0,3*self.Grid),size=(25*self.Grid,2*self.Grid))
-        wx.buttonCameraProperties=wx.Button(wx.tabPageCamera,label="Adjust Camera Properties",
+        self.buttonCameraProperties=wx.Button(self.tabPageCamera,label="Adjust Camera Properties",
                                             pos=(0,5*self.Grid),size=(23*self.Grid,2*self.Grid))
-        wx.labelCameraFPS=wx.StaticText(wx.tabPageCamera,label="Current FPS:",
+        self.labelCameraFPS=wx.StaticText(self.tabPageCamera,label="Current FPS:",
                                         pos=(0,7*self.Grid),size=(8*self.Grid,2*self.Grid))
-        wx.labelCameraFPSValue=wx.StaticText(wx.tabPageCamera,label="0.00",
+        self.labelCameraFPSValue=wx.StaticText(self.tabPageCamera,label="0.00",
                                              pos=(8*self.Grid,7*self.Grid),
                                              size=(6*self.Grid,2*self.Grid))
-        wx.checkBoxCameraFPSLimit=wx.CheckBox(wx.tabPageCamera,label="Limit Frames Per Second",
+        self.checkBoxCameraFPSLimit=wx.CheckBox(self.tabPageCamera,label="Limit Frames Per Second",
                                               pos=(0,9*self.Grid),
                                               size=(16*self.Grid,2*self.Grid))
-        wx.CameraFPSLimit=wx.TextCtrl(wx.tabPageCamera,value="30",
+        self.CameraFPSLimit=wx.TextCtrl(self.tabPageCamera,value="30",
                                       pos=(17*self.Grid,9*self.Grid),
                                       size=(6*self.Grid,2*self.Grid))
-        wx.lblRecord=wx.StaticText(wx.tabPageCamera,label="[Recording]",
+        self.lblRecord=wx.StaticText(self.tabPageCamera,label="[Recording]",
                                    pos=(41*self.Grid,1*self.Grid),
                                    size=(8*self.Grid,2*self.Grid))
-        wx.btnRecord=wx.Button(wx.tabPageCamera,label="RECORD",
+        self.btnRecord=wx.Button(self.tabPageCamera,label="RECORD",
                                pos=(42*self.Grid,3*self.Grid),
                                size=(6*self.Grid,2*self.Grid))
-        wx.btnLoad=wx.Button(wx.tabPageCamera,label="LOAD",
+        self.btnLoad=wx.Button(self.tabPageCamera,label="LOAD",
                              pos=(42*self.Grid,5*self.Grid),
                              size=(6*self.Grid,2*self.Grid))
-        wx.btnView=wx.Button(wx.tabPageCamera,label="VIEW",
+        self.btnView=wx.Button(self.tabPageCamera,label="VIEW",
                              pos=(42*self.Grid,7*self.Grid),
                              size=(6*self.Grid,2*self.Grid))
-        wx.btnClear=wx.Button(wx.tabPageCamera,label="CLEAR",
+        self.btnClear=wx.Button(self.tabPageCamera,label="CLEAR",
                               pos=(42*self.Grid,9*self.Grid),
                               size=(6*self.Grid,2*self.Grid))
         ##构建TabPageTokens
-        wx.buttonMarkerAdd=wx.Button(wx.tabPageTokens,label="New Marker",
+        self.buttonMarkerAdd=wx.Button(self.tabPageTokens,label="New Marker",
                                      pos=(0,0),size=(10*self.Grid,2*self.Grid))
-        wx.comboBoxMarkers=wx.ComboBox(wx.tabPageTokens,value="Edit Existing Marker",
+        self.comboBoxMarkers=wx.ComboBox(self.tabPageTokens,value="Edit Existing Marker",
                                        pos=(11*self.Grid,0),
                                        size=(14*self.Grid,2*self.Grid))
-        wx.lblTotalMarker=wx.StaticText(wx.tabPageTokens,label="Number of markers:",
+        self.lblTotalMarker=wx.StaticText(self.tabPageTokens,label="Number of markers:",
                                         pos=(26*self.Grid,0),
                                         size=(13*self.Grid,2*self.Grid))
-        wx.lblMarkerCount=wx.StaticText(wx.tabPageTokens,label="0",
+        self.lblMarkerCount=wx.StaticText(self.tabPageTokens,label="0",
                                         pos=(40*self.Grid,0),
                                         size=(4*self.Grid,2*self.Grid))
-        wx.buttonMarkerSave=wx.Button(wx.tabPageTokens,label="Save M",
+        self.buttonMarkerSave=wx.Button(self.tabPageTokens,label="Save M",
                                       pos=(45*self.Grid,0),
                                       size=(6*self.Grid,2*self.Grid))
-        wx.buttonMarkerLoad=wx.Button(wx.tabPageTokens,label="Load M",
+        self.buttonMarkerLoad=wx.Button(self.tabPageTokens,label="Load M",
                                       pos=(52*self.Grid,0),
                                       size=(6*self.Grid,2*self.Grid))
-        wx.lblMarkerControl=wx.StaticText(wx.tabPageTokens,label="No Marker Selected",
+        self.lblMarkerControl=wx.StaticText(self.tabPageTokens,label="No Marker Selected",
                                           pos=(0,3*self.Grid),
                                           size=(58*self.Grid,2*self.Grid))
-        wx.buttonMarkerRemove=wx.Button(wx.tabPageTokens,label="Remove This Marker",
+        self.buttonMarkerRemove=wx.Button(self.tabPageTokens,label="Remove This Marker",
                                         pos=(0,5*self.Grid),
                                         size=(20*self.Grid,2*self.Grid))
-        wx.checkBoxMarkerHighlight=wx.CheckBox(wx.tabPageTokens,label="Highlight Marker",
+        self.checkBoxMarkerHighlight=wx.CheckBox(self.tabPageTokens,label="Highlight Marker",
                                                pos=(0,7*self.Grid),
                                                size=(16*self.Grid,2*self.Grid))
-        wx.checkBoxMarkerSmoothing=wx.CheckBox(wx.tabPageTokens,label="Smooth Marker Data",
+        self.checkBoxMarkerSmoothing=wx.CheckBox(self.tabPageTokens,label="Smooth Marker Data",
                                                pos=(0,9*self.Grid),
                                                size=(16*self.Grid,2*self.Grid))
-        wx.labelMarkerThresh=wx.StaticText(wx.tabPageTokens,label="Marker Threshold:",
+        self.labelMarkerThresh=wx.StaticText(self.tabPageTokens,label="Marker Threshold:",
                                            pos=(0,11*self.Grid),
                                             size=(15*self.Grid,2*self.Grid))
-        wx.MarkerThresh=wx.TextCtrl(wx.tabPageTokens,value="0",
+        self.MarkerThresh=wx.TextCtrl(self.tabPageTokens,value="0",
                                     pos=(15*self.Grid,11*self.Grid),
                                     size=(5*self.Grid,2*self.Grid))
-        wx.labelMarkerData=wx.TextCtrl(wx.tabPageTokens,pos=(21*self.Grid,5*self.Grid),
+        self.labelMarkerData=wx.TextCtrl(self.tabPageTokens,pos=(21*self.Grid,5*self.Grid),
                                        size=(37*self.Grid,8*self.Grid))
-        wx.labelMarkerData.SetEditable(False)
+        self.labelMarkerData.SetEditable(False)
         ##构建TabPageApps
-        wx.labelDemoInstructions=wx.TextCtrl(wx.tabPageApps,pos=(44*self.Grid,0),
+        self.labelDemoInstructions=wx.TextCtrl(self.tabPageApps,pos=(44*self.Grid,0),
                                              size=(16*self.Grid,16*self.Grid))
-        wx.labelDemoInstructions.SetEditable(False)
+        self.labelDemoInstructions.SetEditable(False)
 
         #构建Label组
-        wx.labelM=wx.StaticText(self, label=" M", pos=(4*self.Grid,self.Grid),
+        self.labelM=wx.StaticText(self, label=" M", pos=(4*self.Grid,self.Grid),
                                 size=(2*self.Grid,2*self.Grid))
-        wx.labelN=wx.StaticText(self, label=" N", pos=(4*self.Grid,4*self.Grid),
+        self.labelN=wx.StaticText(self, label=" N", pos=(4*self.Grid,4*self.Grid),
                                 size=(2*self.Grid,2*self.Grid))
-        wx.labelO=wx.StaticText(self, label=" O", pos=(1*self.Grid,self.Grid),
+        self.labelO=wx.StaticText(self, label=" O", pos=(1*self.Grid,self.Grid),
                                 size=(2*self.Grid,2*self.Grid))
-        wx.labelP=wx.StaticText(self, label=" P", pos=(1*self.Grid,4*self.Grid),
+        self.labelP=wx.StaticText(self, label=" P", pos=(1*self.Grid,4*self.Grid),
                                 size=(2*self.Grid,2*self.Grid))
-        wx.labelM.SetBackgroundColour("ORANGE RED")
-        wx.labelO.SetBackgroundColour("ORANGE RED")
-        wx.labelN.SetBackgroundColour("RED")
-        wx.labelP.SetBackgroundColour("RED")
-        wx.labelDemoName=wx.StaticText(self, label="WUW", pos=(7*self.Grid,self.Grid),
+        self.labelM.SetBackgroundColour("ORANGE RED")
+        self.labelO.SetBackgroundColour("ORANGE RED")
+        self.labelN.SetBackgroundColour("RED")
+        self.labelP.SetBackgroundColour("RED")
+        self.labelDemoName=wx.StaticText(self, label="WUW", pos=(7*self.Grid,self.Grid),
                                        size=(4*self.Grid,2*self.Grid))
-        wx.lblResult=wx.StaticText(self, label="Test", pos=(12*self.Grid,self.Grid),
+        self.lblResult=wx.StaticText(self, label="Test", pos=(12*self.Grid,self.Grid),
                                    size=(12*self.Grid,2*self.Grid))
-        
+
+#线程——捕获某帧图像 
+
+class ThreadCapture(threading.Thread):
+    def __init__(self, threadname, times, cam):
+        threading.Thread.__init__(self, name=threadname)
+        self.__cam = cam
+        self.__times = times
+        self.__stop = False
+    def run(self):
+        while not self.__stop:
+            self.__cam.ImageCaptured()
+            time.sleep(self.__times)
+    def stop(self):
+        self.__stop = True
+
+#线程——捕获图像显示
+class ThreadShow(threading.Thread):
+    def __init__(self, threadname, times, panel, cam):
+        threading.Thread.__init__(self, name=threadname)
+        self.__times = times
+        self.__panel = panel
+        self.__cam = cam
+        self.__stop = False
+    def run(self):
+        while not self.__stop:
+            img = self.__cam.GetCurrentImage()
+            bmp = TouchlessLib.ImageToBitmap(img)
+            dc = wx.ClientDC(self.__panel.pictureBoxDisplay)
+            dc.DrawBitmap(bmp,0,0)
+            time.sleep(self.__times)
+    def stop(self):
+        self.__stop = True
 
 def main():
     app = wx.App(False)
     frame = wx.Frame(None, wx.ID_ANY, "WUW", size=(WuwPanel.Width,WuwPanel.Height))
     panel = WuwPanel(frame)
     frame.Show(True)
+    cam = TouchlessLib.Camera()
+    cam.Start()
+    threadCapture = ThreadCapture("Capture", 0.03, cam)
+    threadShow = ThreadShow("Show", 0.03, panel, cam)
+    threadCapture.start()
+    threadShow.start()
     app.MainLoop()
+    threadCapture.stop()
+    threadShow.stop()
 
 if __name__ == "__main__":
     main()
